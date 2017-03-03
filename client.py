45254b072f0f2c421e6ba84309dcc8a09a21afbf
dannypete <daniel.peterson@risksense.com>
2017-03-03 15:38:31 -0700

    Initial commit

---
+++ client.py
@@ -0,0 +1,50 @@
+# Dependencies
+import argparse
+import socket
+import sys
+
+# Length of either response.
+# Server response may have spaces in it
+# to adhere to message length
+MESSAGE_LENGTH = 32
+BLOCK_SIZE = 32
+
+def connect(ip, port, block, keyid):
+
+    global MESSAGE_LENGTH, BLOCK_SIZE
+
+    cipher_size = len(block)
+
+    if not(cipher_size % BLOCK_SIZE == 0):
+      print "Bad block(s) size"
+      exit()
+
+    else:
+      # Create a TCP/IP socket
+      sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+      # Connect the socket to the port where the server is listening
+      server_address = (ip, int(port))
+      sock.connect(server_address)
+
+      num_blocks = (cipher_size/BLOCK_SIZE)-1 # num_blocks to be decrypted (IV not counting as ciphertext block)
+
+      try:
+          # Send data
+          message = block + ':' + str(num_blocks) + ':' + keyid  
+          sock.sendall(message)
+
+          # Look for the response
+          amount_received = 0
+          amount_expected = MESSAGE_LENGTH
+
+          while amount_received < amount_expected:
+              data = sock.recv(MESSAGE_LENGTH)
+              amount_received += len(data)
+
+          if "successful" in data:
+            print >>sys.stderr, 'Sending: "%s"' % message
+            print >>sys.stderr, 'Received: "%s"' % data
+
+      finally:
+          sock.close()
---
+++ clientcaller.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import client
+
+sendData = '70344a7648516471334b50656d71536f8d7f660708278e882c0d0c681fe97879bc5ef86a9a3297dc6bd404adfab24b5895f177aeef5d3e29da2225fa405255d2c6e0b89f18f102dbd4a5c0a53519d7b8a8ba1dbf0cd82fbfc39176a3b941af07c0b223d49492c6caef83cad290d069bf69d72da348c0d6f7ad9ad66f15469b6f2f401c0d98f8a8de649748e257688acc6d0d0183e667776cb71379cc1b676b082ef4f00cae59d46b3203c3f0dbbb972a'
+ipaddr = '64.106.46.58'
+port = '10020'
+keyid = '20'
+
+def dec2hex(d):
+    return hex(d).split('x')[1]
+    
+def hex2str(h):
+    result = str(h)
+    if len(result) == 1:
+        return '0' + result
+    else:
+        return result    
+
+for i in range (0,255):
+    client.connect(ipaddr,port,sendData[0:len(sendData) -2] + hex2str(dec2hex(i)),keyid)
+    #print sendData[0:len(sendData) -2] + hex2str(dec2hex(i))
+    
